17abbca6098a4be5a0f44c894cec78a0ee01b8f5
Vadimatorik <Deryabkinda@mail.ru>
2018-03-07 23:15:45 +0700

    Все еще пишется поиск точки входа.

--- main.cpp
+++ main.cpp
@@ -1,18 +1,28 @@
 #include "main.h"
 
+/*!
+ * Ищем строку
+ */
+#define REG_EXP_TEMPLATE_ENTRY_POINT				"[[][*]]\\s-->\\s[^\\s]*"
+
 QString search_entry_point ( char* file_path ) {
 	QString entry_point_func_name;
 	QFile f( file_path );
-	bool r;
-	r = f.open(QIODevice::ReadOnly | QIODevice::Text);
-	if ( !r ) return entry_point_func_name;					/// Точка входа отсутствует.
-	QString b;												/// Строка-буффер.
+	bool b_r;
+	int i_r;
+
+	b_r = f.open(QIODevice::ReadOnly | QIODevice::Text);
+	if ( !b_r ) return entry_point_func_name;				/// Точка входа отсутствует.
+
+	QString	b;												/// Строка-буффер.
+	QRegExp	re_in( REG_EXP_TEMPLATE_ENTRY_POINT );
 	while ( !f.atEnd() ) {
 		b = f.readLine();
-
+		i_r = re_in.indexIn( b );
+		if ( i_r == -1 ) continue;
 	}
 }
 
 int main ( int argc, char* argv[] ) {
-	std::cout << "Hello";
+	search_entry_point( argv[ 1 ] );
 }
